5e406dfc9d479f211b2dddc40c2768db0083775e
JuliaVisconti <julia.visconti@sptech.school>
2025-05-20 14:13:44 -0300

    Anexando a Dashboard API

---
+++ web-data-viz dashboard/src/database/Script do banco de dados.sql
@@ -0,0 +1,39 @@
+
+-- BANCO -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+drop database Entusiastas_da_historia;
+
+create database Entusiastas_da_historia;
+
+use Entusiastas_da_historia;
+
+select * from Usuario;
+
+-- TABELAS -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create table Usuario (
+	id int primary key auto_increment,
+	nome varchar(60),
+    apelido varchar(80),
+	telefone char(13),
+	email varchar(60) unique,
+	senha varchar(80)
+	);
+        
+create table Quiz (
+	id int primary key auto_increment,
+    perguntas int,
+    periodo varchar(30));
+
+create table Tentativa (
+	id int primary key auto_increment,
+    acertos int,
+    erros int,
+    nao_respondidos int,
+    dataHora datetime,
+    fkUsuario int,
+    fkQuiz int,
+    constraint usuarioTentativa FOREIGN KEY (fkUsuario) REFERENCES Usuario(id),
+    constraint quizzTentativa FOREIGN KEY (fkQuiz) REFERENCES Quiz(id)
+    );
+
