a16ca5cb25576fba5b851af7e20408886982be25
JuliaVisconti <julia.visconti@sptech.school>
2025-06-02 18:02:24 -0300

    Atualização do banco de dados

--- src/database/Script do banco de dados.sql
+++ src/database/Script do banco de dados.sql
@@ -32,59 +32,55 @@ create table Resposta (
     dataHora datetime,
     fkUsuario int,
     fkQuestao int,
-    constraint usuarioTentativa FOREIGN KEY (fkUsuario) REFERENCES Usuario(id),
-    constraint questaoTentativa FOREIGN KEY (fkQuestao) REFERENCES Questao(id)
+    constraint usuarioTentativa foreign key (fkUsuario) references Usuario(id),
+    constraint questaoTentativa foreign key (fkQuestao) references Questao(id)
     );
 
-INSERT INTO questao (descricao, resposta) VALUES
-("O que marca o fim da Pré-História?", "D"),
-("Durante o Paleolítico, os humanos:", "C"),
-("Uma conquista crucial do período Paleolítico foi:", "B"),
-("A principal característica do Neolítico foi:", "C"),
-("A Idade dos Metais ficou marcada pela(o):", "A"),
-("A Idade Média começou com:", "C"),
-("O sistema social e econômico dominante na Idade Média era:", "D"),
-("No sistema feudal, os servos:", "C"),
-("O que caracterizou a Baixa Idade Média?", "C"),
-("A Peste Negra causou:", "B"),
-("Um dos marcos iniciais da Idade Moderna foi:", "D"),
-("O Renascimento destacou-se por valorizar:", "A"),
-("A Reforma Protestante foi iniciada por:", "D"),
-("O absolutismo é caracterizado pelo:", "C"),
-("O mercantilismo defendia:", "B"),
-("A Revolução Francesa foi motivada por:", "D"),
-("A Revolução Industrial teve início na:", "A"),
-("A Primeira Guerra Mundial terminou com:", "C"),
-("A Guerra Fria foi marcada por:", "D"),
-("A globalização é caracterizada por:", "A");
+insert into questao (descricao, resposta) values ("O que marca o fim da Pré-História?", "D"),
+	                                             ("Durante o Paleolítico, os humanos:", "C"),
+	                                             ("Uma conquista crucial do período Paleolítico foi:", "B"),
+	                                             ("A principal característica do Neolítico foi:", "C"),
+	                                             ("A Idade dos Metais ficou marcada pela(o):", "A"),
+	                                             ("A Idade Média começou com:", "C"),
+	                                             ("O sistema social e econômico dominante na Idade Média era:", "D"),
+                                                 ("No sistema feudal, os servos:", "C"),
+                                                 ("O que caracterizou a Baixa Idade Média?", "C"),
+                                                 ("A Peste Negra causou:", "B"),
+                                                 ("Um dos marcos iniciais da Idade Moderna foi:", "D"),
+                                                 ("O Renascimento destacou-se por valorizar:", "A"),
+                                                 ("A Reforma Protestante foi iniciada por:", "D"),
+                                                 ("O absolutismo é caracterizado pelo:", "C"),
+                                                 ("O mercantilismo defendia:", "B"),
+                                                 ("A Revolução Francesa foi motivada por:", "D"),
+                                                 ("A Revolução Industrial teve início na:", "A"),
+                                                 ("A Primeira Guerra Mundial terminou com:", "C"),
+                                                 ("A Guerra Fria foi marcada por:", "D"),
+                                                 ("A globalização é caracterizada por:", "A");
 
-SELECT * FROM questao;
+select * from questao;
 
-INSERT INTO usuario VALUES
-(DEFAULT, 'Julia', 'juju', '11987654321', 'juvisconti@sptech.school', 'Urubu#100', null)
+insert into usuario values (default, 'Julia', 'juju', '11987654321', 'juvisconti@sptech.school', 'Urubu#100', null)
 
 
-CREATE VIEW graficoRespostas AS (
-	SELECT
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 1 THEN 1 ELSE 0 END) AS acertoQ1, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 1 THEN 1 ELSE 0 END) AS erroQ1, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 2 THEN 1 ELSE 0 END) AS acertoQ2, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 2 THEN 1 ELSE 0 END) AS erroQ2, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 3 THEN 1 ELSE 0 END) AS acertoQ3, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 3 THEN 1 ELSE 0 END) AS erroQ3, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 4 THEN 1 ELSE 0 END) AS acertoQ4, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 4 THEN 1 ELSE 0 END) AS erroQ4, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 5 THEN 1 ELSE 0 END) AS acertoQ5, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 5 THEN 1 ELSE 0 END) AS erroQ5, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 6 THEN 1 ELSE 0 END) AS acertoQ6, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 6 THEN 1 ELSE 0 END) AS erroQ6, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 7 THEN 1 ELSE 0 END) AS acertoQ7, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 7 THEN 1 ELSE 0 END) AS erroQ7, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 8 THEN 1 ELSE 0 END) AS acertoQ8, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 8 THEN 1 ELSE 0 END) AS erroQ8, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 9 THEN 1 ELSE 0 END) AS acertoQ9, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 9 THEN 1 ELSE 0 END) AS erroQ9, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 10 THEN 1 ELSE 0 END) AS acertoQ10, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 10 THEN 1 ELSE 0 END) AS erroQ10, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 11 THEN 1 ELSE 0 END) AS acertoQ11, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 11 THEN 1 ELSE 0 END) AS erroQ11, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 12 THEN 1 ELSE 0 END) AS acertoQ12, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 12 THEN 1 ELSE 0 END) AS erroQ12, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 13 THEN 1 ELSE 0 END) AS acertoQ13, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 13 THEN 1 ELSE 0 END) AS erroQ13, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 14 THEN 1 ELSE 0 END) AS acertoQ14, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 14 THEN 1 ELSE 0 END) AS erroQ14, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 15 THEN 1 ELSE 0 END) AS acertoQ15, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 15 THEN 1 ELSE 0 END) AS erroQ15, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 16 THEN 1 ELSE 0 END) AS acertoQ16, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 16 THEN 1 ELSE 0 END) AS erroQ16, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 17 THEN 1 ELSE 0 END) AS acertoQ17, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 17 THEN 1 ELSE 0 END) AS erroQ17, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 18 THEN 1 ELSE 0 END) AS acertoQ18, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 18 THEN 1 ELSE 0 END) AS erroQ18, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 19 THEN 1 ELSE 0 END) AS acertoQ19, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 19 THEN 1 ELSE 0 END) AS erroQ19, 
-		SUM(CASE WHEN pontuacao = 1 AND fkQuestao = 20 THEN 1 ELSE 0 END) AS acertoQ20, SUM(CASE WHEN pontuacao = 0 AND fkQuestao = 20 THEN 1 ELSE 0 END) AS erroQ20
-    FROM resposta
-);
+create view graficoRespostas as (select
+	sum(case when pontuacao = 1 and fkQuestao = 1 then 1 else 0 end) as acertoQ1, sum(case when pontuacao = 0 and fkQuestao = 1 then 1 else 0 end) as erroQ1, 
+	sum(case when pontuacao = 1 and fkQuestao = 2 then 1 else 0 end) as acertoQ2, sum(case when pontuacao = 0 and fkQuestao = 2 then 1 else 0 end) as erroQ2, 
+	sum(case when pontuacao = 1 and fkQuestao = 3 then 1 else 0 end) as acertoQ3, sum(case when pontuacao = 0 and fkQuestao = 3 then 1 else 0 end) as erroQ3, 
+	sum(case when pontuacao = 1 and fkQuestao = 4 then 1 else 0 end) as acertoQ4, sum(case when pontuacao = 0 and fkQuestao = 4 then 1 else 0 end) as erroQ4, 
+	sum(case when pontuacao = 1 and fkQuestao = 5 then 1 else 0 end) as acertoQ5, sum(case when pontuacao = 0 and fkQuestao = 5 then 1 else 0 end) as erroQ5, 
+	sum(case when pontuacao = 1 and fkQuestao = 6 then 1 else 0 end) as acertoQ6, sum(case when pontuacao = 0 and fkQuestao = 6 then 1 else 0 end) as erroQ6, 
+	sum(case when pontuacao = 1 and fkQuestao = 7 then 1 else 0 end) as acertoQ7, sum(case when pontuacao = 0 and fkQuestao = 7 then 1 else 0 end) as erroQ7, 
+	sum(case when pontuacao = 1 and fkQuestao = 8 then 1 else 0 end) as acertoQ8, sum(case when pontuacao = 0 and fkQuestao = 8 then 1 else 0 end) as erroQ8, 
+	sum(case when pontuacao = 1 and fkQuestao = 9 then 1 else 0 end) as acertoQ9, sum(case when pontuacao = 0 and fkQuestao = 9 then 1 else 0 end) as erroQ9, 
+	sum(case when pontuacao = 1 and fkQuestao = 10 then 1 else 0 end) as acertoQ10, sum(case when pontuacao = 0 and fkQuestao = 10 then 1 else 0 end) as erroQ10, 
+	sum(case when pontuacao = 1 and fkQuestao = 11 then 1 else 0 end) as acertoQ11, sum(case when pontuacao = 0 and fkQuestao = 11 then 1 else 0 end) as erroQ11, 
+	sum(case when pontuacao = 1 and fkQuestao = 12 then 1 else 0 end) as acertoQ12, sum(case when pontuacao = 0 and fkQuestao = 12 then 1 else 0 end) as erroQ12, 
+	sum(case when pontuacao = 1 and fkQuestao = 13 then 1 else 0 end) as acertoQ13, sum(case when pontuacao = 0 and fkQuestao = 13 then 1 else 0 end) as erroQ13, 
+	sum(case when pontuacao = 1 and fkQuestao = 14 then 1 else 0 end) as acertoQ14, sum(case when pontuacao = 0 and fkQuestao = 14 then 1 else 0 end) as erroQ14, 
+	sum(case when pontuacao = 1 and fkQuestao = 15 then 1 else 0 end) as acertoQ15, sum(case when pontuacao = 0 and fkQuestao = 15 then 1 else 0 end) as erroQ15, 
+	sum(case when pontuacao = 1 and fkQuestao = 16 then 1 else 0 end) as acertoQ16, sum(case when pontuacao = 0 and fkQuestao = 16 then 1 else 0 end) as erroQ16, 
+	sum(case when pontuacao = 1 and fkQuestao = 17 then 1 else 0 end) as acertoQ17, sum(case when pontuacao = 0 and fkQuestao = 17 then 1 else 0 end) as erroQ17, 
+	sum(case when pontuacao = 1 and fkQuestao = 18 then 1 else 0 end) as acertoQ18, sum(case when pontuacao = 0 and fkQuestao = 18 then 1 else 0 end) as erroQ18, 
+	sum(case when pontuacao = 1 and fkQuestao = 19 then 1 else 0 end) as acertoQ19, sum(case when pontuacao = 0 and fkQuestao = 19 then 1 else 0 end) as erroQ19, 
+	sum(case when pontuacao = 1 and fkQuestao = 20 then 1 else 0 end) as acertoQ20, sum(case when pontuacao = 0 and fkQuestao = 20 then 1 else 0 end) as erroQ20
+    from resposta);
